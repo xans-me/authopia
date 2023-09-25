1ea2747509a1e05e778781408c5928fe689ea151
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2023-09-25 23:38:55 +0700

    need to customize error the grpc

--- helpers/response/models.go
+++ helpers/response/models.go
@@ -8,8 +8,9 @@ type Struct struct {
 
 // ErrorStruct model response
 type ErrorStruct struct {
-	Code    int
-	Message string
+	ErrorCode   int    `json:"errorCode"`
+	Description string `json:"description"`
+	Message     string `json:"message"`
 }
 
 // we implement the built-in package 'error' interface by creating this function
--- src/users/constants.go
+++ src/users/constants.go
@@ -22,22 +22,22 @@ var (
 
 // error variable default values
 var (
-	ErrGeneral            = response.ErrorStruct{Code: 1, Message: "internal server error"}
-	ErrDecodeRequestData  = response.ErrorStruct{Code: 2, Message: "decode request data failed"}
-	ErrBusinessLogic      = response.ErrorStruct{Code: 3, Message: "business logic execution failed"}
-	ErrUserCredential     = response.ErrorStruct{Code: 4, Message: "credential mismatch"}
-	ErrEmailUnverified    = response.ErrorStruct{Code: 5, Message: "email is not yet verified"}
-	ErrGetUserInfo        = response.ErrorStruct{Code: 6, Message: "get user info failed"}
-	ErrVerifyOTP          = response.ErrorStruct{Code: 7, Message: "otp code is invalid"}
-	ErrUniqueUser         = response.ErrorStruct{Code: 8, Message: "users must be unique"}
-	ErrActionEmail        = response.ErrorStruct{Code: 9, Message: "execute action email failed"}
-	ErrWrongPassword      = response.ErrorStruct{Code: 10, Message: "wrong password"}
-	ErrPhoneIsExist       = response.ErrorStruct{Code: 11, Message: "phone number is already exist"}
-	ErrEmailIsExist       = response.ErrorStruct{Code: 12, Message: "email is already exist"}
-	ErrCantSendOTP        = response.ErrorStruct{Code: 13, Message: "cannot resend otp yet"}
-	ErrExpiredOTP         = response.ErrorStruct{Code: 14, Message: "otp code is expired"}
-	ErrInvalidPhoneNumber = response.ErrorStruct{Code: 15, Message: "invalid phone number"}
-	ErrSamePhoneNumber    = response.ErrorStruct{Code: 16, Message: "cannot use old phone numbers"}
-	ErrCouchDBNotFound    = response.ErrorStruct{Code: 17, Message: "DB : user not found / available to creating new"}
-	ErrExistCouchDBUser   = response.ErrorStruct{Code: 18, Message: "DB : user already exist"}
+	ErrGeneral            = response.ErrorStruct{ErrorCode: 1, Message: "internal server error"}
+	ErrDecodeRequestData  = response.ErrorStruct{ErrorCode: 2, Message: "decode request data failed"}
+	ErrBusinessLogic      = response.ErrorStruct{ErrorCode: 3, Message: "business logic execution failed"}
+	ErrUserCredential     = response.ErrorStruct{ErrorCode: 4, Message: "credential mismatch"}
+	ErrEmailUnverified    = response.ErrorStruct{ErrorCode: 5, Message: "email is not yet verified"}
+	ErrGetUserInfo        = response.ErrorStruct{ErrorCode: 6, Message: "get user info failed"}
+	ErrVerifyOTP          = response.ErrorStruct{ErrorCode: 7, Message: "otp code is invalid"}
+	ErrUniqueUser         = response.ErrorStruct{ErrorCode: 8, Message: "users must be unique"}
+	ErrActionEmail        = response.ErrorStruct{ErrorCode: 9, Message: "execute action email failed"}
+	ErrWrongPassword      = response.ErrorStruct{ErrorCode: 10, Message: "wrong password"}
+	ErrPhoneIsExist       = response.ErrorStruct{ErrorCode: 11, Message: "phone number is already exist"}
+	ErrEmailIsExist       = response.ErrorStruct{ErrorCode: 12, Message: "email is already exist"}
+	ErrCantSendOTP        = response.ErrorStruct{ErrorCode: 13, Message: "cannot resend otp yet"}
+	ErrExpiredOTP         = response.ErrorStruct{ErrorCode: 14, Message: "otp code is expired"}
+	ErrInvalidPhoneNumber = response.ErrorStruct{ErrorCode: 15, Message: "invalid phone number"}
+	ErrSamePhoneNumber    = response.ErrorStruct{ErrorCode: 16, Message: "cannot use old phone numbers"}
+	ErrCouchDBNotFound    = response.ErrorStruct{ErrorCode: 17, Message: "DB : user not found / available to creating new"}
+	ErrExistCouchDBUser   = response.ErrorStruct{ErrorCode: 18, Message: "DB : user already exist"}
 )
